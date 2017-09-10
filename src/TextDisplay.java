01253443534e822fdb06088a191e8bdbe6f3438d
Eric Tang <erictang1729@gmail.com>
2017-09-09 23:24:35 -0700

    Files in src folder now

---
+++ src/TextDisplay.java
@@ -0,0 +1,10 @@
+
+public class TextDisplay 
+{
+	//So this TextDisplay will just add lines of text . . .
+	//And the CheckersGUIPruning will take a TextDisplay as input, which it will modify (add lines to)
+	//And we'll create a MasterClass to create the combined CheckersGUIPruning and TextDisplay
+	
+	
+
+}
